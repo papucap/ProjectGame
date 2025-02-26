d478347e9b32dd68f1a5aa97bf2a7084b8441d66
papuc <papucapatrik@gmail.com>
2025-02-26 15:21:57 +0100

    Trida TextGameWorld

--- src/TextGameWorld.java
+++ src/TextGameWorld.java
@@ -1,2 +1,19 @@
+import java.io.IOException;
+
 public class TextGameWorld {
+    public static void main(String[] args) throws IOException {
+        World world = new World();
+        world.loadWorld("world.csv");
+
+        // Testovací výpis
+        for (String roomName : world.rooms.keySet()) {
+            System.out.println("Místnost: " + roomName);
+            for (String direction : new String[]{"north", "south", "east", "west"}) {
+                Room exit = world.rooms.get(roomName).getExit(direction);
+                if (exit != null) {
+                    System.out.println("  " + direction + " -> " + exit.name);
+                }
+            }
+        }
+    }
 }
