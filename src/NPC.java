d1d4c4fbf81cb0b425677b5417ca08a2453e0cd7
papuc <papucapatrik@gmail.com>
2025-03-03 17:14:01 +0100

    Update tridy NPC

--- src/NPC.java
+++ src/NPC.java
@@ -1,2 +1,13 @@
 public class NPC {
+    String name;
+    Quest quest;
+
+    public NPC(String name, Quest quest) {
+        this.name = name;
+        this.quest = quest;
+    }
+
+    public void interact(){
+
+    }
 }
