9b0bf3cc690f82b99dd3599bdd303c1c88015da4
papuc <papucapatrik@gmail.com>
2025-03-03 17:14:49 +0100

    Update tridy Quest

--- src/Quest.java
+++ src/Quest.java
@@ -1,2 +1,11 @@
 public class Quest {
+    private String description;
+
+    public Quest(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
 }
