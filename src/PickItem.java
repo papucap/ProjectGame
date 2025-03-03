55bebb17f2d183bc489d7eeb28929e6d0825df61
papuc <papucapatrik@gmail.com>
2025-03-03 17:21:20 +0100

    Pridani Command a tridy PickItem

---
+++ src/PickItem.java
@@ -0,0 +1,14 @@
+public class PickItem {
+    private Inventory inventory;
+    private Item item;
+
+    public PickItem(Inventory inventory, Item item) {
+        this.inventory = inventory;
+        this.item = item;
+    }
+
+    public void execute() {
+        inventory.addItem(item);
+    }
+
+}
