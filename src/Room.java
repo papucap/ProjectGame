bf4db594d5a3318a632d4dbe6267f6eb2cc899e6
papuc <papucapatrik@gmail.com>
2025-03-03 17:12:13 +0100

    Pridani Trid Item a NPC a pridani metod do Room

--- src/Room.java
+++ src/Room.java
@@ -1,13 +1,20 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+
 
 public class Room {
     String name;
     Map<String, Room> exits;
+    List<NPC> npcs;
+    List<Item> items;
 
     public Room(String name) {
         this.name = name;
         this.exits = new HashMap<>();
+        this.npcs = new ArrayList<>();
+        this.items = new ArrayList<>();
     }
 
     public void setExit(String direction, Room room) {
@@ -16,5 +23,17 @@ public class Room {
 
     public Room getExit(String direction) {
         return exits.get(direction);
+    }
+
+    public void addNPC(NPC npc) {
+        npcs.add(npc);
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void interactNPC() {
+
     }
 }
